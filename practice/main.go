fdcede9b4758063d5b49c192820ab30031c16f2c
shivam1903 <shivam234agarwal@gmail.com>
2024-11-25 18:48:05 +0530

    Adding changes for string conversion

--- practice/main.go
+++ practice/main.go
@@ -4,6 +4,8 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
@@ -12,5 +14,15 @@ func main() {
 	fmt.Print("Enter text: ")
 	input, _ := reader.ReadString('\n')
 	fmt.Println("You entered:", input)
+
+	fmt.Print("Enter number: ")
+	inputInt, _ := reader.ReadString('\n')
+	numFloat, err := strconv.ParseFloat(strings.TrimSpace(inputInt), 64)
+	if err != nil{
+		fmt.Println(err)
+	} else{
+		fmt.Println("Given number = ", numFloat)
+	}
+
 
 }
