6aacf77d82099978feb461a7ae402e2d7b8585ad
shivam1903 <shivam234agarwal@gmail.com>
2024-11-25 20:22:57 +0530

    Working with Slices

--- practice/main.go
+++ practice/main.go
@@ -2,9 +2,26 @@ package main
 
 import (
 	"fmt"
+	"sort"
 )
 
 func main() {
-	var colors = [3]string{"Red", "Green", "Blue"}
+	var colors = []string{"Red", "Green", "Blue"}
 	fmt.Println(colors)
+	colors = append(colors, "Purple")
+	fmt.Println(colors)
+
+	colors = append(colors[0:len(colors)-1])
+	fmt.Println(colors)
+
+	numbers := make([]int, 5)
+	numbers[0] = 134
+	numbers[1] = 23
+	numbers[2] = 78
+	numbers[3] = -19
+	fmt.Println(numbers)
+
+	sort.Ints(numbers)
+	fmt.Println(numbers)
+
 }
