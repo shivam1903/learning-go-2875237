dd920966f4b2dfae7310568338e59aa79a5949c7
shivam1903 <shivam234agarwal@gmail.com>
2024-11-25 18:03:02 +0530

    making input reading file

--- practice/main.go
+++ practice/main.go
@@ -2,12 +2,19 @@ package main
 
 import (
 	"fmt"
+	"bufio"
+	"os"
 )
 
 func main() {
 
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Enter Text ")
+	input, _ := reader.ReadString('\n')
+	fmt.Println("You entered: ", input)
+
 	var aString string = "This is Go!"
 	fmt.Println(aString)
-	fmt.Printf("The variable's type is %T\n", aString)
+	fmt.Printf("The variable's type is %T\n", input)
 
 }
