1e72314de8e193eece0a539b229465e30ea57b84
shivam1903 <shivam234agarwal@gmail.com>
2024-11-25 19:11:18 +0530

    Working with the time package

--- practice/main.go
+++ practice/main.go
@@ -2,10 +2,20 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
 func main() {
 
+	n := time.Now()
+	fmt.Println("The current time date is ", n)
 	fmt.Println("Dates and times")
+
+	t := time.Date(2009, time.November, 10, 10,10, 0, 0, time.UTC)
+	fmt.Println("Random time created is ", t)
+	fmt.Println(t.Format(time.ANSIC))
+
+	parsedTime, _ := time.Parse(time.ANSIC, "Tue Nov 10 10:10:00 2009")
+	fmt.Printf("The type of parsedTime is %T\n", parsedTime)
 
 }
