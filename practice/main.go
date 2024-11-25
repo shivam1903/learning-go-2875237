febcd1a35e66124b66dd5309ef29c6946918a623
shivam1903 <shivam234agarwal@gmail.com>
2024-11-25 20:05:44 +0530

    Working with Arrays

--- practice/main.go
+++ practice/main.go
@@ -5,5 +5,18 @@ import (
 )
 
 func main() {
-	fmt.Println("Arrays")
+
+	var colors [3]string
+
+	colors[0] = "red"
+	colors[1] = "green"
+	colors[2] = "blue"
+
+	fmt.Println("This is the array ", colors)
+
+	var number = [5]int64{1,2,3,4,5}
+	fmt.Println("this is the number array ", number)
+
+	fmt.Println("Number of numbers ", len(number))
+
 }
