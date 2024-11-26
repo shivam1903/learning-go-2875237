4f656430c8d9f099121a23ebd89da91c37a9aee6
shivam1903 <shivam234agarwal@gmail.com>
2024-11-26 16:32:34 +0530

    Working with switch commands

--- practice/main.go
+++ practice/main.go
@@ -10,4 +10,22 @@ func main() {
 	rand.Seed(time.Now().Unix())
 	dow := rand.Intn(7) + 1
 	fmt.Println("Day", dow)
+
+	var res string
+
+	switch dow{
+	case 1:
+		res = "It is Sunday"
+		// fallthrough
+		// fallthrough is used when we want to move to the next case, just like skip
+	
+	case 2:
+		res = "It's monday"
+	
+	default:
+		res = "It is a weekday"
+
+	}
+
+	fmt.Println(res)
 }
