c5a19a4098ced68c198585e9fb60b3c60608ad7c
shivam1903 <shivam234agarwal@gmail.com>
2024-11-26 22:59:11 +0530

    Working with methods

--- practice/main.go
+++ practice/main.go
@@ -5,14 +5,21 @@ import (
 )
 
 func main() {
-	poodle := Dog{"Poodle", 10}
+	poodle := Dog{"Poodle", 10, "Haww"}
 	fmt.Println(poodle)
 	fmt.Printf("%+v\n", poodle)
 	fmt.Printf("Breed: %v\nWeight: %v\n", poodle.Breed, poodle.Weight)
+
+	poodle.Speak()
 }
 
 // Dog is a struct
 type Dog struct {
 	Breed  string
 	Weight int
+	Sound string
+}
+
+func (d Dog) Speak() {
+	fmt.Println(d.Sound)
 }
